b40bd92cdc4eb8d746f69490b98295a8fe4836c1
ITSMijaiL <ITSMijaiL@users.noreply.github.com>
2021-05-05 08:32:33 -0400

    Update InstallMijaiLsDE.sh

--- InstallMijaiLsDE.sh
+++ InstallMijaiLsDE.sh
@@ -32,29 +32,30 @@ fi
 
 
 echo "[0/5]Doing some preparations ..."
-sudo mkdir /home/MijaiLsDE
-sudo mkdir /home/MijaiLsDE/DebPKGS
+sudo mkdir /usr/local/bin/MijaiLsDE
+sudo mkdir /usr/local/bin/MijaiLsDE/DebPKGS
 echo "[1/5]Installing packages (apt-get) ..."
 sudo apt-get install xfwm4 lxqt-panel
 sudo snap install wonderwall
 echo "[2/5]Downloading & installing packages (wget) ..."
-sudo wget https://github.com/Ulauncher/Ulauncher/releases/download/5.11.0/ulauncher_5.11.0_all.deb -P /home/MijaiLsDE/DebPKGS
-sudo apt install /home/MijaiLsDE/DebPKGS/ulauncher_5.11.0_all.deb
-echo "[3/5]Making DE initialization script in /home/MijaiLsDE/MDE.sh ..."
-sudo touch /home/MijaiLsDE/MDE.sh
-sudo cat > MDE.sh << "EOF"
+sudo wget https://github.com/Ulauncher/Ulauncher/releases/download/5.11.0/ulauncher_5.11.0_all.deb -P /usr/local/bin/MijaiLsDE/DebPKGS
+sudo apt install /usr/local/bin/MijaiLsDE/DebPKGS/ulauncher_5.11.0_all.deb
+echo "[3/5]Making DE initialization script in /usr/local/bin/MijaiLsDE/MDE.sh ..."
+sudo touch /usr/local/bin/MijaiLsDE/MDE.sh
+sudo cat > /usr/local/bin/MijaiLsDE/MDE.sh << "EOF"
 exec xfwm4
 exec lxqt-panel
 exec ulauncher
 exec wonderwall
 EOF
+sudo chmod +x /usr/local/bin/MijaiLsDE/MDE.sh
 echo "[4/5]Making the .desktop file (/usr/share/xsessions/)"
 sudo touch /usr/share/xsessions/MDE.desktop
 sudo cat > /usr/share/xsessions/MDE.desktop << "EOF"
 [Desktop Entry]
 Name=MijaiLsDesktopEnvironment
 Comment=Or what i like to call it: a strange frankenstein of packages
-Exec=/home/MijaiLsDE/MDE.sh
+Exec=/usr/local/bin/MijaiLsDE/MDE.sh
 Type=Application
 EOF
 echo "[5/5] Done! Restart or logout to initiate the DE."
