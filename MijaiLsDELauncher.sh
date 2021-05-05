3d56ca7f023e6672257c592f242f91583d0e0fd4
ITSMijaiL <ITSMijaiL@users.noreply.github.com>
2021-05-05 13:10:07 -0400

    Added an auto updater for the launcher

--- MijaiLsDELauncher.sh
+++ MijaiLsDELauncher.sh
@@ -1,4 +1,29 @@
 #!/bin/bash
+LauncherVersion="1.1"
+VersionFromGithub=$( curl -# -L "https://raw.githubusercontent.com/ITSMijaiL/MijaiLsDE/main/Version.txt" 2> '/dev/null' )
+
+if [ "$EUID" -ne 0 ]
+then 
+	echo "To download the DE installer its necessary to be root!"
+  	exit
+fi
+
+if  [  "$LauncherVersion" = "$VersionFromGithub" ];
+then
+	echo "Launcher is up-to-date!"
+else
+	echo "Launcher is not up-to-date! Downloading the new launcher and exitting..."
+	sudo rm -i $(pwd)/MijaiLsDELauncher.sh
+	sudo wget https://raw.githubusercontent.com/ITSMijaiL/MijaiLsDE/main/MijaiLsDELauncher.sh -P $(pwd)
+	exit
+fi
+
+if ! command -v curl &> /dev/null
+then
+    echo "curl could not be found ..."
+    exit
+fi
+
 if ! command -v apt-get &> /dev/null
 then
     echo "apt-get could not be found ..."
@@ -15,12 +40,6 @@ if ! command -v wget &> /dev/null
 then
     echo "wget could not be found ..."
     exit
-fi
-
-if [ "$EUID" -ne 0 ]
-then 
-	echo "To download the DE installer its necessary to be root!"
-  	exit
 fi
 sudo mkdir /usr/local/bin/MijaiLsDE
 #Download the installer script
