074d2e1325b70ca5659da0425e7ea61082f1738b
ITSMijaiL <ITSMijaiL@users.noreply.github.com>
2021-05-05 08:15:44 -0400

    Create UninstallMijaiLsDE.sh

---
+++ UninstallMijaiLsDE.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if ! command -v apt-get &> /dev/null
+then
+    echo "apt-get could not be found ..."
+    exit
+fi
+
+if ! command -v snap &> /dev/null
+then
+    echo "snap could not be found ..."
+    exit
+fi
+
+if [ "$EUID" -ne 0 ]
+then 
+	echo "To uninstall the DE is necessary to be root too!"
+  	exit
+fi
+
+echo "[1/2]Deleting the folder (${HOME}/MijaiLsDE) ..."
+sudo rm -rf $HOME/MijaiLsDE
+echo "[2/2]Deleting the .desktop file (/usr/share/xsessions/)"
+sudo rm -i /usr/share/xsessions/MDE.desktop
+echo "Done! Packages need to be removed manually since they can be in your system and can be essential for your main DE to run."
+echo "List of packages to remove manually and optionally:"
+echo "(using apt-get) dwm, lxqt-panel"
+echo "(using snap) wonderwall"
+echo "(using apt) ulauncher"
+
