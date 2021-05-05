e0b6bc3c4686a89bf67006cadde2c781438f2930
ITSMijaiL <ITSMijaiL@users.noreply.github.com>
2021-05-05 09:50:56 -0400

    Create MijaiLsDELauncher.sh
    Wooo, now i don't have to copy and paste the code from this repo lol

---
+++ MijaiLsDELauncher.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+if ! command -v apt-get &> /dev/null
+then
+    echo "apt-get could not be found ..."
+    exit
+fi
+
+if ! command -v snap &> /dev/null
+then
+    echo "snap could not be found ..."
+    exit
+fi
+
+if ! command -v wget &> /dev/null
+then
+    echo "wget could not be found ..."
+    exit
+fi
+
+if [ "$EUID" -ne 0 ]
+then 
+	echo "To download the DE installer its necessary to be root!"
+  	exit
+fi
+cd $HOME
+#Download the installer script
+sudo wget https://raw.githubusercontent.com/ITSMijaiL/MijaiLsDE/main/InstallMijaiLsDE.sh
+#Download the uninstaller
+sudo wget https://raw.githubusercontent.com/ITSMijaiL/MijaiLsDE/main/UninstallMijaiLsDE.sh
+#Run it
+sudo bash InstallMijaiLsDE.sh
+#Profit?
+
